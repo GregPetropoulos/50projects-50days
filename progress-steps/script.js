c7d637d0c2e3ba60829f46aa5f4e8de36b2fd822
GP <gregpetropoulos@yahoo.com>
2022-08-10 12:50:17 -0400

    Finished js logic  for progress steps.

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,1 +1,50 @@
-const btnPrev=document.querySelectorAll('.btn');
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+//circles is a NodeList, an array of 4 divs with class = 'circle'
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', () => {
+  currentActive++;
+  //keep from going past array length of 4
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+
+  update();
+});
+
+prev.addEventListener('click', () => {
+  currentActive--;
+  //keep from going past array length of 4
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+  const actives = document.querySelectorAll('.active');
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
